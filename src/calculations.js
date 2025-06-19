0e7812a30eb8bbf5af35ade4e88959448bed7f9f
ruagit <justin.bosman@skybettingandgaming.com>
2025-06-19 11:08:08 +0100

    Add calculations and type handles, add templates

---
+++ src/calculations.js
@@ -0,0 +1,59 @@
+const heatLosskWh = (floorArea, heatingFactor, insulationFactor) => {
+  // floorArea (m^2) * heatingFactor * insulationFactor = heat loss (kWh)
+
+  if(typeof floorArea !== 'number' || typeof heatingFactor !== 'number' || typeof insulationFactor !== 'number') {
+    throw new Error("Invalid Heat Loss input: All parameters must be numbers.");
+  }
+  return (floorArea * heatingFactor * insulationFactor);
+};
+
+const powerHeatLossKw = (heatLosskWhTotal, heatingDegreeDays) => { 
+  //  heat loss (kWh) / heating degree days = Power heat loss (kW)
+
+  if(typeof heatLosskWh !== 'number' || typeof heatingDegreeDays !== 'number') {
+    throw new Error("Invalid Power Heat Loss inputs: All parameters must be numbers." );
+  }
+
+  return (heatLosskWhTotal / heatingDegreeDays);
+};    
+
+const heatPumpSelection = (powerHeatLossKwTotal, heatPumpOptions) => {  
+
+  if(!Array.isArray(heatPumpOptions) || heatPumpOptions.length === 0) {
+    throw new Error("Invalid Heat Pump Options: Must be a non-empty array.");
+  }
+
+  if(typeof powerHeatLossKwTotal !== 'number') {
+    throw new Error("Invalid Power Heat Loss input: Must be a number.");
+  }
+
+  for (const heatPump of heatPumpOptions) {
+    if (heatPump.outputCapacity >= powerHeatLossKwTotal) {
+      return heatPump;
+    }
+  }
+  throw new Error ("No suitable heat pump found for the given power heat loss.");
+}
+
+const totalCost = (heatPump, vatRate) => {
+  if (typeof heatPump !== 'object' || typeof vatRate !== 'number') {
+    throw new Error("Invalid Cost inputs: All parameters must be valid types.");
+  }
+  let grossCost;
+
+  for (const item of heatPump.costs) {
+     grossCost += item.cost;
+  }
+
+  const vatAmount = grossCost * vatRate;
+  const totalCostWithVat = grossCost + vatAmount;
+
+  return totalCostWithVat;
+ };
+
+ module.exports = {
+  heatLosskWh,
+  powerHeatLossKw,
+  heatPumpSelection,
+  totalCost
+};
--- src/summaryTemplates.js
+++ src/summaryTemplates.js
@@ -1,23 +1,32 @@
 const outputSummaryTemplate = (submissionId, estimatedHeatLoss, designRegion, powerHeatLoss, recommendedHeatPump, totalCost) => {
+  console.log(' ');
   console.log('--------------------------------------');
   console.log(submissionId);
   console.log('--------------------------------------');
+  console.log(' ');
   console.log(`  Estimated Heat Loss       = ${estimatedHeatLoss}`);
   console.log(`  Design Region             = ${designRegion}`);
   console.log(`  Power Heat Loss           = ${powerHeatLoss}`);
   console.log(`  Recommended Heat Pump     = ${recommendedHeatPump.label}`);
   console.log('  Cost Breakdown');
+  console.log(' ');
   for (const item of recommendedHeatPump.costs) {
     console.log(`    ${item.label} ${item.cost}`);
   }
+  console.log(' ');
   console.log(`  Total Cost, including VAT = ${totalCost}`);
+  console.log(' ');
 }
 
-const errorSummaryTemplate = (submissionId, errorMessage) => {  
+const errorSummaryTemplate = (submissionId, heatingLosskWhTotal, errorMessage) => { 
+  console.log(' ');
   console.log('--------------------------------------');
   console.log(submissionId);
   console.log('--------------------------------------');
-  console.log(`Error: ${errorMessage}`);
+  console.log(' ');
+  console.log(`Heating Loss: ${heatingLosskWhTotal}`);
+  console.log(`Warning: ${errorMessage}`);
+  console.log(' ');
 };
 
 module.exports = {
